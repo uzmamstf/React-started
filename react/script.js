487500d4f28c220d4b0cbba6e77da928c8eed7fc
uzmamstf <49521264+uzmamstf@users.noreply.github.com>
2019-09-03 18:17:16 +0500

    Add files via upload

---
+++ react/script.js
@@ -0,0 +1,30 @@
+// console.log("Hello React!!");
+// let isRaining = false
+// if (isRaining == true)
+//     console.log("use umbrella");
+// else{
+//     console.log("wear sunglasses")
+// }
+
+// let myObject={
+//     "name": "object"
+// }
+const camelCase = require('camelcase');
+
+console.log(camelCase('foo_bar'));
+
+const boxen = require('boxen');
+
+console.log(boxen('Haye maze', {padding: 1}));
+console.log(boxen("game over", {borderColor
+    : 'red'}))
+
+// abc.info("hello ");
+
+// console.log("This is a log information");
+// console.warn("Warning!");
+// console.info("Information");
+// console.table([[1, 2], [3, 4]]);
+// console.time("Timer");
+// console.timeEnd("Timer");
+// console.dir(myObject);
